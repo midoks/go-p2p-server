72912c12397af90cf98f131fc104fe879bf3a775
midoks <midoks@163.com>
2021-11-05 15:17:18 +0800

    up

--- internal/cmd/service.go
+++ internal/cmd/service.go
@@ -1,6 +1,13 @@
 package cmd
 
 import (
+	"fmt"
+	"net/http"
+
+	go_logger "github.com/phachon/go-logger"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/urfave/cli"
 )
 
@@ -8,14 +15,54 @@ var Service = cli.Command{
 	Name:        "service",
 	Usage:       "This command starts P2P services",
 	Description: `Start Web P2P Server services`,
-	Action:      runAllService,
+	Action:      RunService,
 	Flags: []cli.Flag{
 		stringFlag("config, c", "", "Custom configuration file path"),
 	},
 }
 
-func runAllService(c *cli.Context) error {
+var logger *go_logger.Logger
 
-	// app.Start(conf.Web.HttpPort)
+//websocket
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+//websocket实现
+func websocketReqMethod(c *gin.Context) {
+	//升级get请求为webSocket协议
+	ws, err := upGrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	defer ws.Close()
+
+	for {
+
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			logger.Errorf("read websocket msg: %v", err)
+			break
+		}
+
+		fmt.Println(mt, message, err)
+
+	}
+}
+
+func RunService(c *cli.Context) error {
+
+	httpPort := "3030"
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World")
+	})
+
+	r.GET("/ws", websocketReqMethod)
+
+	r.Run(fmt.Sprintf(":%s", httpPort))
 	return nil
 }
